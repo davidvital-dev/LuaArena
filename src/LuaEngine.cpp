ab63e1e62b159199bb189b78ceaf90acc3790062
David J. <david.vital@aluno.ufca.edu.br>
2026-08-11 09:08:14 -0300

    feat: gerencia ciclo de vida do lua_State

--- src/LuaEngine.cpp
+++ src/LuaEngine.cpp
@@ -1,9 +1,24 @@
 #include "LuaEngine.hpp"
 
-LuaEngine::LuaEngine() noexcept
-    : state_(nullptr) {}
+extern "C" {
+#include <lauxlib.h>
+#include <lua.h>
+#include <lualib.h>
+}
 
-LuaEngine::~LuaEngine() noexcept = default;
+LuaEngine::LuaEngine() noexcept
+    : state_(luaL_newstate()) {
+    if (state_ != nullptr) {
+        luaL_openlibs(state_);
+    }
+}
+
+LuaEngine::~LuaEngine() noexcept {
+    if (state_ != nullptr) {
+        lua_close(state_);
+        state_ = nullptr;
+    }
+}
 
 bool LuaEngine::isInitialized() const noexcept {
     return state_ != nullptr;
