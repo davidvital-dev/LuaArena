a283e01c884019a3461fcc9d9fa6264d03b7a6f7
David J. <david.vital@aluno.ufca.edu.br>
2026-08-11 09:09:07 -0300

    feat: implementa carregamento protegido de scripts Lua

--- src/LuaEngine.cpp
+++ src/LuaEngine.cpp
@@ -24,6 +24,45 @@ bool LuaEngine::isInitialized() const noexcept {
     return state_ != nullptr;
 }
 
+bool LuaEngine::loadScript(const std::string& scriptPath) {
+    lastError_.clear();
+
+    if (state_ == nullptr) {
+        lastError_ = "estado Lua não inicializado";
+        return false;
+    }
+
+    if (scriptPath.empty()) {
+        lastError_ = "caminho do script Lua não pode ser vazio";
+        return false;
+    }
+
+    const int stackTop = lua_gettop(state_);
+
+    if (luaL_loadfile(state_, scriptPath.c_str()) != LUA_OK) {
+        const char* message = lua_tostring(state_, -1);
+        lastError_ = "falha ao carregar script '" + scriptPath + "': "
+            + (message == nullptr ? "erro Lua desconhecido" : message);
+        lua_settop(state_, stackTop);
+        return false;
+    }
+
+    if (lua_pcall(state_, 0, 0, 0) != LUA_OK) {
+        const char* message = lua_tostring(state_, -1);
+        lastError_ = "erro ao executar script '" + scriptPath + "': "
+            + (message == nullptr ? "erro Lua desconhecido" : message);
+        lua_settop(state_, stackTop);
+        return false;
+    }
+
+    lua_settop(state_, stackTop);
+    return true;
+}
+
+const std::string& LuaEngine::getLastError() const noexcept {
+    return lastError_;
+}
+
 lua_State* LuaEngine::getState() noexcept {
     return state_;
 }
